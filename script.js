25680c4569ecc0d56d7125c161f213cb1a53218b
seshan-b <seshan.b@gmail.com>
2022-06-25 17:34:34 +1200

    Edited Scripts

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.querySelectorAll(".story-btn").forEach(btn =>
 
     btn.addEventListener("click", () => {
         //console.log("Clicked"); Testing
+        btn.classList.toggle("change-button-on-click");
     }
 
 )
