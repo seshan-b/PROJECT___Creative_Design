8e74257507c8a76ee58c60fb9e27e14745fc3ea4
seshan-b <seshan.b@gmail.com>
2022-06-26 17:35:06 +1200

    Edited Scripts

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.querySelectorAll(".story-btn").forEach(btn =>
     btn.addEventListener("click", () => {
         //console.log("Clicked"); Testing
         btn.classList.toggle("change-button-on-click");
+        btn.nextElementSibling.classList.toggle("display-story-on-click");
     }
 
 )
