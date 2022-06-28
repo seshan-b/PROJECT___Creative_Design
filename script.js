3c20108bd271e041eaa85021a4b21e802d814519
seshan-b <seshan.b@gmail.com>
2022-06-28 17:39:46 +1200

    Edited Scripts

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-document.querySelectorAll(".story-btn").forEach(btn => {
-    
-    btn.addEventListener("click", () => {
-//console.log("Clicked"); Testing
-btn.classList.toggle("change-button-on-click");
-btn.nextElementSibling.classList.toggle("display-story-on-click");
-    })
-
+document.querySelectorAll('.story-btn').forEach((btn) => {
+	btn.addEventListener('click', () => {
+		//console.log("Clicked"); Testing
+		btn.classList.toggle('change-button-on-click')
+		btn.nextElementSibling.classList.toggle('display-story-on-click')
+	})
 })
-
