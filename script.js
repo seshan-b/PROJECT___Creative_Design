26e7acb878d6b09c8e5ec26377295c1281882609
seshan-b <seshan.b@gmail.com>
2022-07-05 17:26:09 +1200

    Clean-up Script

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.querySelectorAll('.story-btn').forEach((btn) => {
 	btn.addEventListener('click', () => {
-		//console.log("Clicked"); Testing
+		// console.log("Clicked"); Testing
 		btn.classList.toggle('change-button-on-click')
+
 		btn.nextElementSibling.classList.toggle('display-story-on-click')
 	})
 })
